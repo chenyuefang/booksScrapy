613e79a9be232e833bab4863a1f924f44b31116d
Administrator <416326391@qq.com>
2019-05-14 11:20:37 +0800

    alter

--- booksScrapy/spiders/books_spider.py
+++ booksScrapy/spiders/books_spider.py
@@ -1,11 +1,16 @@
-from scrapy.spiders import Spider # 爬虫模式
-from scrapy import Request # 请求模式
+
+from scrapy.spiders import Spider  # 爬虫模式
+from scrapy import Request  # 请求模式
+
 
 class booksSpider(Spider):
-    name = 'books' # 爬虫的名字
+    name = 'books'  # 爬虫的名字
     start_urls = ["http://books.toscrapr.com/"]
 
     # 爬虫功能
-    def parse(self,response):
+    def parse(self, response):
         print(response.text)
-        # 数据提取功能
+
+    # 数据提取功能
+    li_selector = response.expath("//oi/ol")
+
