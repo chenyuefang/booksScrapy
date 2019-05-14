443d8aa24b70f75a4ed5a2dcecad4d5998ccf81b
Administrator <416326391@qq.com>
2019-05-14 10:29:52 +0800

    Initial commit

---
+++ booksScrapy/spiders/books_spider.py
@@ -0,0 +1,11 @@
+from scrapy.spiders import Spider # 爬虫模式
+from scrapy import Request # 请求模式
+
+class booksSpider(Spider):
+    name = 'books' # 爬虫的名字
+    start_urls = ["http://books.toscrapr.com/"]
+
+    # 爬虫功能
+    def parse(self,response):
+        print(response.text)
+        # 数据提取功能
