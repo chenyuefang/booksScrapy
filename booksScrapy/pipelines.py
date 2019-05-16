e7834f873ffe9d01626efc97e7ba9ade268dabc4
Administrator <416326391@qq.com>
2019-05-16 09:33:49 +0800

    alter

--- booksScrapy/pipelines.py
+++ booksScrapy/pipelines.py
@@ -1,20 +1,25 @@
-# -*- coding: utf-8 -*-
 
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymongo
 
-
-class BooksscrapyPipeline(object):
+class MybooksPipeline(object):
+    #爬虫开始前执行一次（初始化的工作）
+    def open_spider(self,spider):
+        # 连接MongoDb数据库服务器
+        self.db_client = pymongo.MongoClient()
+        # 指定操作的数据库
+        self.db = self.db_client["scrape"]
+        # 指定集合（类似MYSQL中的表）
+        self.db_collection = self.db["books"]
 
     def process_item(self, item, spider):
-        # 处理数据
-        with open("books.txt","a",encoding="utf-8") as f:
-            book_str = item["name"] + ":" +item["price"] + "\n"
-            f.write(book_str)
+        #处理数据
+        self.db_collection.insert_one(dict(item))
+        return item
 
-         return item
-    # 爬虫全部完成后执行一次
-    def close_spider(self):
-        pass
+    #爬虫全部完成后执行一次（收尾工作）
+    def close_spider(self,spider):
+        self.db_client.close()
