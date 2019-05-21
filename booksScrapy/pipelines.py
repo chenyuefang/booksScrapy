4805753c1c7effe7639c792fde20f262757e9d64
Administrator <416326391@qq.com>
2019-05-21 10:28:21 +0800

    alter

--- booksScrapy/pipelines.py
+++ booksScrapy/pipelines.py
@@ -1,13 +1,13 @@
-
 # Define your item pipelines here
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import pymongo
 
+
 class MybooksPipeline(object):
-    #爬虫开始前执行一次（初始化的工作）
-    def open_spider(self,spider):
+    # 爬虫开始前执行一次（初始化的工作）
+    def open_spider(self, spider):
         # 连接MongoDb数据库服务器
         self.db_client = pymongo.MongoClient()
         # 指定操作的数据库
@@ -16,10 +16,10 @@ class MybooksPipeline(object):
         self.db_collection = self.db["books"]
 
     def process_item(self, item, spider):
-        #处理数据
+        # 处理数据
         self.db_collection.insert_one(dict(item))
         return item
 
-    #爬虫全部完成后执行一次（收尾工作）
-    def close_spider(self,spider):
+    # 爬虫全部完成后执行一次（收尾工作）
+    def close_spider(self, spider):
         self.db_client.close()
--- start.py
+++ start.py
@@ -2,3 +2,9 @@ from scrapy import cmdline
 
 # 执行文件
 cmdline.execute("scrapy crawl books  ".split())  # -o: output<输出>   books.csv： 自动生成并保存到 books.csv
+
+
+
+# 创建项目目录：
+
+   #  cmd *  scapy startproject booksScrapy
